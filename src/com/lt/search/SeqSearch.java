b150e5c9bf360ec18aed15a7aecb1166d58c3e3a
LT3211 <1377981923@qq.com>
2019-10-13 15:46:35 +0800

    格式化代码

--- src/com/lt/search/SeqSearch.java
+++ src/com/lt/search/SeqSearch.java
@@ -24,7 +24,7 @@ public class SeqSearch {
     public static int seqSearch(int []arr,int value){
         //线性查找是逐一比对，发现有相同值，就返回下标
         for (int i=0;i<arr.length;i++){
-            if (arr[i]==value){
+            if (arr[i] == value) {
                 return i;
             }
         }
